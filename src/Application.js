baa51b1e9f75d3e6dc54e493ac4916247681e04b
Dhruv Bindoria <dhruv.bindoria@gmail.com>
2022-05-21 07:58:20 +0530

    Working on tabs

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React from "react"
+import * as React from "react"
 import { connect } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./Pages/HomePage";
@@ -6,11 +6,41 @@ import CategoryPage from "./Pages/CategoryPage";
 import ManagePage from "./Pages/ManagePage";
 import CssBaseline from '@mui/material/CssBaseline';
 import { Container } from "@mui/material"
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+
+import _ from "underscore"
 
 const Application = (props) => {
+
+    const [value, setValue] = React.useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <>
             <CssBaseline />
+            <Box sx={{ width: '100%', typography: 'body1' }}>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={handleChange} aria-label="lab API tabs example">
+                            {
+                                !_.isEmpty(props.categories) &&
+                                Object.keys(props.categories).map(id => {
+                                    if (props.categories[id].shouldBeListed) {
+                                        return <Tab label={props.categories[id].categoryName} value={id} />
+                                    }
+                                })
+                            }
+                            <Tab label="Manage" value="manage" />
+                        </TabList>
+                    </Box>
+                </TabContext>
+            </Box>
             <Container maxWidth="lg">
                 <Router>
                     <Routes>
@@ -24,6 +54,8 @@ const Application = (props) => {
     )
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+    categories: state.manage.categories
+});
 
 export default connect(mapStateToProps, {})(Application);
